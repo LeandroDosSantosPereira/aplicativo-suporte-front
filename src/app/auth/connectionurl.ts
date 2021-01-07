b32c0cca9154164e864a64a6694f2f2f15cdc166
Leandro <leandro.pereira@novaandradina.org>
2021-01-06 22:33:52 -0300

    comment line code refresh function

--- src/app/auth/connectionurl.ts
+++ src/app/auth/connectionurl.ts
@@ -1,6 +1,6 @@
 export class ConnectionUrl{
-  // public urlconnection: string  = "http://localhost:3000/"   
-  public urlconnection: string  = "https://api-suporte.herokuapp.com/"   
+  public urlconnection: string  = "http://localhost:3000/"   
+  // public urlconnection: string  = "https://api-suporte.herokuapp.com/"   
   
   // Comando de push no Heroku
   // git push heroku HEAD:master  
